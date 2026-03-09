7f088cd272ee1aff870548e8e2c4ffbb99ff8137
ivan-gtz <ivangutierz20@gmail.com>
2026-03-09 19:47:47 -0400

    filled information in paypal checkout

--- src/components/orders/OrderStatus.tsx
+++ src/components/orders/OrderStatus.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
-import { IoCardOutline } from 'react-icons/io5';
+import { MdOutlineCreditScore } from "react-icons/md";
+import { MdOutlineCreditCardOff } from "react-icons/md";
+
 
 interface Props {
     isPaid: boolean;
@@ -9,17 +11,17 @@ export const OrderStatus = ({ isPaid }: Props) => {
     return (
         <div className={
             clsx(
-                "flex flex-row items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                "flex flex-row items-center justify-center border rounded-full py-1 px-4 text-sm font-medium mb-2",
                 {
-                    'bg-red-500': !isPaid,
-                    'bg-green-700': isPaid
+                    'border-amber-700 bg-amber-50 text-amber-700': !isPaid,
+                    'border-emerald-700 bg-green-50 text-lime-700': isPaid
                 }
             )
         }>
-            <IoCardOutline size={30} />
+            {isPaid ? <MdOutlineCreditScore size={30} /> : <MdOutlineCreditCardOff size={30} /> }
             <span className="mx-2">
                 {
-                    isPaid ? 'Orden pagada' : 'Pendiente de pago'
+                    isPaid ? 'Orden ya fue pagada' : 'Pendiente de pago'
                 }
             </span>
         </div>
